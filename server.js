50b752342cf5fcd47e7f8789231c28e8cb43bc30
atolat <atolat@uci.edu>
2016-04-09 06:40:16 -0700

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -25,6 +25,25 @@ app.get('/todos',function(req,res){
     res.json(todos);
 })
 //GET /todos/:id
+app.get('/todos/:id',function(req,res){
+    var todoId=parseInt(req.params.id);
+    var matchTodo;
+   // res.send("Todo item with id: "+todoId);
+    
+    for(var i=0; i<todos.length; i++){
+        if(todos[i].id===todoId){
+            matchTodo=todos[i];
+        }
+    }
+    if(!matchTodo){
+        res.status(404).send();
+    }
+    res.json(matchTodo);
+    
+    
+});
+
+
 
 app.listen(PORT,function(){
    console.log('Express listening on port:: '+PORT);
