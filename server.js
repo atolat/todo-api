55a83f022d15a9949b11024c074d03cf3934868b
atolat <atolat@uci.edu>
2016-04-09 18:20:54 -0700

    Refactored Code using underscoreJS

--- server.js
+++ server.js
@@ -1,56 +1,65 @@
-var express = require('express');
-var bodyParser= require('body-parser');
+    var express = require('express');
+    var bodyParser= require('body-parser');
+    var _=require('underscore');
 
-var app = express();
-var PORT = process.env.PORT || 3000;
-var todos=[];
-var todoId=1;
+    var app = express();
+    var PORT = process.env.PORT || 3000;
+    var todos=[];
+    var todoId=1;
 
-app.use(bodyParser.json());
+    app.use(bodyParser.json());
 
-app.get('/',function(req, res){
-    res.send('Todo API Root');
-});
+    app.get('/',function(req, res){
+        res.send('Todo API Root');
+    });
 
-//GET request to fetch todos
-//GET /todos
-app.get('/todos',function(req,res){
-    res.json(todos);
-})
-//GET /todos/:id
-app.get('/todos/:id',function(req,res){
-    var todoId=parseInt(req.params.id);
-    var matchTodo;
-   // res.send("Todo item with id: "+todoId);
-    
-    for(var i=0; i<todos.length; i++){
-        if(todos[i].id===todoId){
-            matchTodo=todos[i];
+    //GET request to fetch todos
+    //GET /todos
+    app.get('/todos',function(req,res){
+        res.json(todos);
+    })
+    //GET /todos/:id
+    app.get('/todos/:id',function(req,res){
+        var todoId=parseInt(req.params.id);
+        var matchTodo= _.findWhere(todos,{id: todoId});
+       // res.send("Todo item with id: "+todoId);
+
+    //    for(var i=0; i<todos.length; i++){
+    //        if(todos[i].id===todoId){
+    //            matchTodo=todos[i];
+    //        }
+    //    }
+        if(!matchTodo){
+            res.status(404).send();
         }
-    }
-    if(!matchTodo){
-        res.status(404).send();
-    }
-    res.json(matchTodo);
-    
-    
-});
+        res.json(matchTodo);
 
-//POST request to pass user data to app in JSON
-// POST /todos
-app.post('/todos',function(req,res){
-    var body = req.body;
-    body.id=todoId;
-    //console.log(body.description);
-    if(req.body!=={}){
-        todos.push(req.body);
-        todoId++;
-    }
-    res.json(body);
-    
-});
 
-app.listen(PORT,function(){
-   console.log('Express listening on port:: '+PORT);
-    
-})
+    });
+
+    //POST request to pass user data to app in JSON
+    // POST /todos
+    app.post('/todos',function(req,res){
+        var body = _.pick(req.body,'description','completed'); 
+       
+        //Validation
+        if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length===0){
+            return res.status(400).send();
+        }
+
+
+
+        //console.log(body.description);
+         body.description=body.description.trim();
+         body.id=todoId;
+            todos.push(body);
+            todoId++;
+
+        res.json(body);
+
+    });
+
+    app.listen(PORT,function(){
+       console.log('Express listening on port:: '+PORT);
+
+    })
